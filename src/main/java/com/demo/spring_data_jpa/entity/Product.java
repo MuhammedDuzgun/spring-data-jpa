bf818171fb8f4eb2c47faab662453d522b7fbefa
MuhammedDuzgun <muhammedduzgun00@gmail.com>
2024-09-13 21:40:46 +0300

    add Product entity

---
+++ src/main/java/com/demo/spring_data_jpa/entity/Product.java
@@ -0,0 +1,52 @@
+package com.demo.spring_data_jpa.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "products",
+        uniqueConstraints = {
+        @UniqueConstraint(
+                name = "sku_unique",
+                columnNames = "stock_unit"
+        )
+        }
+)
+public class Product {
+    @Id
+    @GeneratedValue(
+            strategy = GenerationType.SEQUENCE,
+            generator = "product_id_generator"
+    )
+    @SequenceGenerator(
+            name = "product_id_generator",
+            sequenceName = "product_id_sequence",
+            allocationSize = 1
+    )
+    private Long id;
+    @Column(name = "stock_unit", nullable = false)
+    private String sku;
+    @Column(nullable = false)
+    private String name;
+    private String description;
+    private BigDecimal price;
+    private boolean active;
+    private String imageUrl;
+    @CreationTimestamp
+    private LocalDateTime dateCreated;
+    @UpdateTimestamp
+    private LocalDateTime dateUpdated;
+
+}
