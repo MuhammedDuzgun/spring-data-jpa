6f98b28b0868887e7b34b2b0659bf4903a73e434
MuhammedDuzgun <muhammedduzgun00@gmail.com>
2024-09-16 21:12:33 +0300

    Add custom queries to Product Repository

--- src/main/java/com/demo/spring_data_jpa/repository/IProductRepository.java
+++ src/main/java/com/demo/spring_data_jpa/repository/IProductRepository.java
@@ -3,8 +3,17 @@ package com.demo.spring_data_jpa.repository;
 import com.demo.spring_data_jpa.entity.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.math.BigDecimal;
 import java.util.List;
+import java.util.Optional;
 
 public interface IProductRepository extends JpaRepository<Product, Long> {
-    List<Product> findByName(String name);
+    Product findByName(String name);
+    Optional<Product> findById(Long id);
+    List<Product> findByNameOrDescription(String name, String description);
+    List<Product> findByNameAndDescription(String name, String description);
+    Product findDistinctByName(String name);
+    List<Product> findByPriceGreaterThan(BigDecimal price);
+    List<Product> findByPriceLessThan(BigDecimal price);
+    Product findByNameContaining(String name);
 }
